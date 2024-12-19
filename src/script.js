3ecce78436fd3bb64d4cda1c86a55c8b988c0030
dylanjamaludin <jamaludindylanedu@gmail.com>
2024-12-19 13:24:28 -0500

    added dark mode, light mode, and os mode capabilities.

--- src/script.js
+++ src/script.js
@@ -4,17 +4,76 @@ const header = document.getElementById("header");
 const menu_button = document.querySelector("button[title='Menu']");
 const menu_exit_button = document.querySelector("button[title='Exit']");
 
-const bell_button= document.querySelector("button[title='Notifications']");
-const bell_icon = document.getElementById('test');
+const bell_button= document.querySelectorAll(".notif");
 const dropdown = document.getElementById("dropdown");
 const dropdown_button = document.querySelector("button[title='More']");
 const body_div = document.querySelector("body>div");
 let bell_toggle_val = 0;
 
+const dark_mode_button = document.querySelector("button[title='Dark Mode']");
+const light_mode_button = document.querySelector("button[title='Light Mode']");
+const os_mode_button = document.querySelector("button[title='OS Mode']");
+
+
+
+const theme_mode = localStorage.getItem("theme_mode");
+function load_theme(mode)
+{
+    localStorage.setItem("theme_mode", mode);
+    let color;
+    if(mode == "os_preference")
+    {
+        color = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark':'light'
+    }
+    else
+    {
+        color = mode=='dark_mode' ? 'dark':'light'
+    }
+
+
+    if((document.body.classList.contains("dark") && color == 'light')||(document.body.classList.contains("light") && color == 'dark'))
+    {
+        document.body.classList.remove("dark");
+        document.body.classList.remove("light");
+        switch (color) {
+            
+            case "dark":
+                document.body.classList.add("dark");
+                break;
+        
+            default:
+                document.body.classList.add("light");
+                break;
+        }
+    }
+}
+
+if(theme_mode)
+{
+    load_theme(theme_mode)
+}
+else
+{
+    load_theme("os_preference");
+}
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event=>
+    {
+        if(localStorage.getItem("theme_mode")== "os_preference")
+        {
+            load_theme('os_preference');
+        }
+        
+    }
+)
+dark_mode_button.onclick = ()=>{load_theme("dark_mode")};
+light_mode_button.onclick = ()=>{load_theme ("light_mode")};
+os_mode_button.onclick = ()=>{load_theme("os_preference")};
+
+
 function dropFunct ()
 {
     dropdown.classList.toggle('p-3');
-    dropdown.classList.toggle('h-0')
+    dropdown.classList.toggle('h-0');
     dropdown.classList.toggle('h-auto');
     
 }
@@ -22,7 +81,7 @@ function dropFunct ()
 
 function menuFunctOpen ()
 {
-    sidebar.classList.remove('w-0')
+    sidebar.classList.remove('w-0');
     sidebar.classList.add('w-60');
     sidebar.classList.toggle('opened');
     header.classList.toggle("blur-sm");
@@ -30,7 +89,7 @@ function menuFunctOpen ()
 }
 function menuFunctExit ()
 {
-    sidebar.classList.remove('w-60')
+    sidebar.classList.remove('w-60');
     sidebar.classList.add('w-0');
     sidebar.classList.toggle('opened');
     dashboard.classList.toggle("blur-sm");
@@ -39,19 +98,37 @@ function menuFunctExit ()
 }
 function changeBellIcon()
 {
-   
+
     if(bell_toggle_val ==0)
     {
-        bell_icon.setAttribute("data-feather","bell-off");
+        for(var i = 0; i < 2; i++)
+        {
+            var test = bell_button[i];
+            console.log(i);
+            test.innerHTML = '<i data-feather="bell-off" class="inline-block stroke-[3px] size-5"></i>';
+
+        }
         bell_toggle_val=1;
+        feather.replace();
+
+
     }
     else if(bell_toggle_val == 1)
     {
-        bell_icon.setAttribute("data-feather","");
+        for(var i = 0; i < 2; i++)
+            {
+                var test = bell_button[i];
+                console.log(i);
+                test.innerHTML = '<i data-feather="bell" class="inline-block stroke-[3px] size-5"></i>';
+    
+            }
         bell_toggle_val = 0;
+        feather.replace(); 
     }
 }
-bell_button.onclick = changeBellIcon;
+
+bell_button[0].onclick = changeBellIcon;
+bell_button[1].onclick = changeBellIcon;
 menu_button.onclick = menuFunctOpen;
 menu_exit_button.onclick = menuFunctExit;
 dropdown_button.onclick =dropFunct;
